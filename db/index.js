98bc4f13c76db0708ca0556da5a92a5e7f2fed2b
江远强 <oncwnuH4P-2sxX_kRfgkuSZI_UuY@git.weixin.qq.com>
2020-05-10 18:21:32 +0800

    finish crud1

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false)
 // mongoose.connect('mongodb://47.107.182.109/stdbreak');
-mongoose.connect('mongodb://139.224.131.108/culture', { useNewUrlParser: true },(err,res)=>{
+mongoose.connect('mongodb://139.224.131.108/culture', { 
+    useNewUrlParser: true ,
+    useUnifiedTopology: true
+},(err,res)=>{
     if(err){
         console.log(err)
     }
